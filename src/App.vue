87987a29ce0ca089d619f76522d376914b703218
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2023-12-18 13:59:31 -0500

    splashscreen created

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
+  <SplashScreen />
   <HomePage />
 </template>
 
-<script setup>
-import HomePage from './components/HomePage.vue'
+<script>
+import HomePage from '@/components/HomePage.vue'
+import { defineAsyncComponent } from 'vue'
+export default {
+  components: {
+    HomePage,
+    SplashScreen: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import('./components/SplashScreen.vue'))
+          }, 1000)
+        })
+    )
+  }
+}
 </script>
 
-<style></style>
+<style>
+html,
+body,
+#app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="splashscreen">
+    <img src="@/assets/cash-flow-app-icon.svg" alt="CashFlow Logo" />
+    <h1><span>Cash</span>Flow</h1>
+    <a class="get-started">Get Started</a>
+  </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  animation: lazy-loading 1500ms linear;
+  margin-top: 0px;
+  opacity: 1;
+}
+
+@keyframes lazy-loading {
+  0% {
+    margin-top: 100px;
+    opacity: 0.1;
+  }
+}
+
+.get-started {
+  padding: 8px 18px;
+  border-radius: 8px;
+  background-color: #b9efff;
+  color: var(--brand-blue);
+  cursor: pointer;
+  font-weight: normal;
+  transition: background-color 200ms ease;
+}
+.get-started:hover {
+  background-color: #cbf3ff;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
